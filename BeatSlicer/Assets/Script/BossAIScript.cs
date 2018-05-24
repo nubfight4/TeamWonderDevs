18f5091a27321bb29c66b1c5138cfa8d31542cb1
Wongfive <0112273@kdu-online.com>
2018-05-24 23:52:06 +0800

    - Added 'Simple' Boss AI and stage to test in

---
+++ BeatSlicer/Assets/Script/BossAIScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAIScript : MonoBehaviour 
+{
+	public GameObject player;
+
+	private Rigidbody bossRigidbody;
+	private Vector3 bossMovementVelocity;
+	private float bossMovementDirection = 3.0f;
+	private float defaultDirection = 1.0f;
+
+	public GameObject bullet;
+	#pragma warning disable 0414
+	private GameObject newBullet;
+	#pragma warning restore 0414
+	public Transform bossBulletHardPoint;
+
+	public float nextFire = 2.0f;
+	private float nextFireTemp;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		bossRigidbody = GetComponent<Rigidbody>();
+	}
+
+
+	void Start()
+	{
+		nextFireTemp = nextFire;
+	}
+
+
+	void Update()
+	{
+		bossAIShootingFunction();
+
+		if(defaultDirection < bossMovementDirection)
+		{
+			defaultDirection += 1.0f * Time.deltaTime;
+		}
+		else
+		{
+			bossMovementFunction();
+
+			if(Random.value > 0.5f)
+			{
+				bossMovementDirection += Random.value;
+			}
+			else
+			{
+				bossMovementDirection -= Random.value;
+			}
+
+			if(Random.value < 1.0f)
+			{
+				bossMovementDirection = 1 + Random.value;
+			}
+
+			defaultDirection = 0.0f;
+		}
+
+		bossRigidbody.velocity = bossMovementVelocity;
+	}
+
+
+	void bossAIShootingFunction()
+	{
+		nextFireTemp -= Time.deltaTime;
+		transform.LookAt(player.transform);
+
+		if(nextFireTemp <= 0)
+		{
+			nextFireTemp = nextFire;
+			newBullet = Instantiate(bullet, bossBulletHardPoint.position, bossBulletHardPoint.rotation);
+			newBullet = null;
+		}
+	}
+
+
+	void bossMovementFunction()
+	{
+		if(Random.value > 0.5f)
+		{
+			bossMovementVelocity.x = 2.0f * Random.value;
+		}
+		else
+		{
+			bossMovementVelocity.x = -2.0f * Random.value;
+		}
+
+		if(Random.value > 0.5f)
+		{
+			bossMovementVelocity.z = 2.0f * Random.value;
+		}
+		else
+		{
+			bossMovementVelocity.z = -2.0f * Random.value;
+		}
+	}
+
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Boundary")
+		{
+			bossMovementVelocity = -bossMovementVelocity.normalized;
+			bossMovementVelocity = bossMovementVelocity * 2.0f;
+		}
+	}
+}
---
+++ BeatSlicer/Assets/Script/BulletScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour 
+{
+	public GameObject player;
+
+	public float bulletSpeed = 10.0f;
+	public float selfDestructTimer = 5.0f;
+
+	private Rigidbody bulletRigidbody;
+	private SphereCollider bulletSphereCollider; //  For Future Use, Probably :)
+
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		bulletRigidbody = GetComponent<Rigidbody>();
+		bulletSphereCollider = GetComponent<SphereCollider>();
+	}
+		
+
+	void Start() 
+	{
+		bulletRigidbody.velocity = transform.forward * bulletSpeed;
+	}
+
+
+	void Update() 
+	{
+		transform.LookAt(player.transform.position);
+		selfDestructFunction();
+	}
+
+
+	void selfDestructFunction()
+	{
+		selfDestructTimer -= Time.deltaTime;
+
+		if(selfDestructTimer <= 0)
+		{
+			Destroy(this.gameObject); 
+		}
+	}
+		
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(this.bulletSphereCollider.enabled == true)
+		{
+			if(other.tag == "Player")
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
