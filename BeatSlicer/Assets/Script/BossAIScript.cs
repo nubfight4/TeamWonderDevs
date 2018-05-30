84172029ef73e5bdac8d641191b54e3ea97bf598
Wongfive <0112273@kdu-online.com>
2018-05-30 10:40:11 +0800

    - Added different coloured bullets - Added a simple pattern - Some bullet Materials - A.I. Script

--- BeatSlicer/Assets/Script/BossAIScript.cs
+++ BeatSlicer/Assets/Script/BossAIScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class BossAIScript : MonoBehaviour 
 {
+	enum attackPattern
+	{
+		attackPattern1,
+		attackPattern2,
+		attackPattern3,
+		standby
+	};
+		
 	public GameObject player;
 
 	private Rigidbody bossRigidbody;
@@ -11,14 +19,26 @@ public class BossAIScript : MonoBehaviour
 	private float bossMovementDirection = 3.0f;
 	private float defaultDirection = 1.0f;
 
-	public GameObject bullet;
-	#pragma warning disable 0414
+	public GameObject bulletRed;
+	public GameObject bulletGreen;
+	public GameObject bulletBlue;
+
+	#pragma warning disable 0414 // Disable Warning
 	private GameObject newBullet;
-	#pragma warning restore 0414
+	#pragma warning restore 0414 // Reenable Warning
+
 	public Transform bossBulletHardPoint;
+
+	//For Testing
+	public Transform bossBulletHardPointM;
+	public Transform bossBulletHardPointL;
+	public Transform bossBulletHardPointR;
 
 	public float nextFire = 2.0f;
 	private float nextFireTemp;
+	attackPattern currentAttackPattern = attackPattern.standby;
+	//attackPattern previousAttackPattern = attackPattern.standby; // To Be Used Eventually
+
 
 	void Awake()
 	{
@@ -30,21 +50,97 @@ public class BossAIScript : MonoBehaviour
 	void Start()
 	{
 		nextFireTemp = nextFire;
+		currentAttackPattern = attackPattern.attackPattern1;
 	}
 
 
 	void Update()
 	{
 		bossAIShootingFunction();
+		bossMovementFunction();
+	}
 
+
+	void bossAIShootingFunction()
+	{
+		nextFireTemp -= Time.deltaTime;
+		transform.LookAt(player.transform);
+		Vector3 eulerAngles = transform.rotation.eulerAngles;
+		eulerAngles = new Vector3(0, eulerAngles.y, 0);
+		transform.rotation = Quaternion.Euler(eulerAngles);
+
+		if(nextFireTemp <= 0)
+		{
+			nextFireTemp = nextFire;
+			setAttackPattern();
+			newBullet = null;
+		}
+	}
+
+
+	void setAttackPattern()
+	{
+		if(currentAttackPattern == attackPattern.attackPattern1)
+		{
+			newBullet = Instantiate(bulletRed, bossBulletHardPoint.position, bossBulletHardPoint.rotation);
+			newBullet = Instantiate(bulletRed, bossBulletHardPointM.position, bossBulletHardPointM.rotation);
+			newBullet = Instantiate(bulletRed, bossBulletHardPointL.position, bossBulletHardPointL.rotation);
+			newBullet = Instantiate(bulletRed, bossBulletHardPointR.position, bossBulletHardPointR.rotation);
+
+			currentAttackPattern = attackPattern.attackPattern2;
+			//previousAttackPattern = attackPattern.attackPattern1;
+		}
+		else if(currentAttackPattern == attackPattern.attackPattern2)
+		{
+			newBullet = Instantiate(bulletGreen, bossBulletHardPoint.position, bossBulletHardPoint.rotation);
+			newBullet = Instantiate(bulletGreen, bossBulletHardPointM.position, bossBulletHardPointM.rotation);
+			newBullet = Instantiate(bulletGreen, bossBulletHardPointL.position, bossBulletHardPointL.rotation);
+			newBullet = Instantiate(bulletGreen, bossBulletHardPointR.position, bossBulletHardPointR.rotation);
+
+			currentAttackPattern = attackPattern.attackPattern3;
+			//previousAttackPattern = attackPattern.attackPattern2;
+		}
+		else if(currentAttackPattern == attackPattern.attackPattern3)
+		{
+			newBullet = Instantiate(bulletBlue, bossBulletHardPoint.position, bossBulletHardPoint.rotation);
+			newBullet = Instantiate(bulletBlue, bossBulletHardPointM.position, bossBulletHardPointM.rotation);
+			newBullet = Instantiate(bulletBlue, bossBulletHardPointL.position, bossBulletHardPointL.rotation);
+			newBullet = Instantiate(bulletBlue, bossBulletHardPointR.position, bossBulletHardPointR.rotation);
+
+			currentAttackPattern = attackPattern.attackPattern1;
+			//previousAttackPattern = attackPattern.attackPattern3;
+		}
+	}
+
+
+	void bossMovementFunction()
+	{
 		if(defaultDirection < bossMovementDirection)
 		{
 			defaultDirection += 1.0f * Time.deltaTime;
 		}
 		else
 		{
-			bossMovementFunction();
+			//For Velocity Changes
+			if(Random.value > 0.5f)
+			{
+				bossMovementVelocity.x = 2.0f * Random.value;
+			}
+			else
+			{
+				bossMovementVelocity.x = -2.0f * Random.value;
+			}
 
+			if(Random.value > 0.5f)
+			{
+				bossMovementVelocity.z = 2.0f * Random.value;
+			}
+			else
+			{
+				bossMovementVelocity.z = -2.0f * Random.value;
+			}
+
+			//For Direction Changes
 			if(Random.value > 0.5f)
 			{
 				bossMovementDirection += Random.value;
@@ -63,42 +159,6 @@ public class BossAIScript : MonoBehaviour
 		}
 
 		bossRigidbody.velocity = bossMovementVelocity;
-	}
-
-
-	void bossAIShootingFunction()
-	{
-		nextFireTemp -= Time.deltaTime;
-		transform.LookAt(player.transform);
-
-		if(nextFireTemp <= 0)
-		{
-			nextFireTemp = nextFire;
-			newBullet = Instantiate(bullet, bossBulletHardPoint.position, bossBulletHardPoint.rotation);
-			newBullet = null;
-		}
-	}
-
-
-	void bossMovementFunction()
-	{
-		if(Random.value > 0.5f)
-		{
-			bossMovementVelocity.x = 2.0f * Random.value;
-		}
-		else
-		{
-			bossMovementVelocity.x = -2.0f * Random.value;
-		}
-
-		if(Random.value > 0.5f)
-		{
-			bossMovementVelocity.z = 2.0f * Random.value;
-		}
-		else
-		{
-			bossMovementVelocity.z = -2.0f * Random.value;
-		}
 	}
 
 
