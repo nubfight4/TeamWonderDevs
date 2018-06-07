8aa3b28cd4f100d052d020df205f4124dd2a899c
KEBS\User <nubfight@gmail.com>
2018-06-07 22:52:26 +0800

    Change Color Space to Linear, Added Game Manager, Manager Controller and Sound Manager.

---
+++ BeatSlicer/Assets/Script/Managers/GameManagerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour 
+{
+	#region Singleton
+	private static GameManagerScript mInstance;
+
+	public static GameManagerScript Instance
+	{
+		get
+		{
+			if(mInstance == null)
+			{
+				GameManagerScript temp = ManagerControllerScript.Instance.gameManager;
+
+				if(temp == null)
+				{
+					temp = Instantiate(ManagerControllerScript.Instance.gameManagerPrefab, Vector3.zero, Quaternion.identity).GetComponent<GameManagerScript>();
+				}
+				mInstance = temp;
+				ManagerControllerScript.Instance.gameManager = mInstance;
+				DontDestroyOnLoad(mInstance.gameObject);
+			}
+			return mInstance;
+		}
+	}
+	public static bool CheckInstanceExist()
+	{
+		return mInstance;
+	}
+	#endregion Singleton
+
+
+	void Awake()
+	{
+		if(GameManagerScript.CheckInstanceExist())
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void Start()
+	{
+
+	}
+}
---
+++ BeatSlicer/Assets/Script/Managers/ManagerControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerControllerScript : MonoBehaviour 
+{
+	#region Singleton
+	private static ManagerControllerScript mInstance = null;
+
+	public static ManagerControllerScript Instance
+	{
+		get
+		{
+			if(mInstance == null)
+			{
+				GameObject tempObject = GameObject.FindWithTag("GameController");
+
+				if(tempObject == null)
+				{
+					Debug.Break();
+				}
+				else
+				{
+					mInstance = tempObject.GetComponent<ManagerControllerScript>();
+				}
+			}
+			return mInstance;
+		}
+	}
+	public static bool CheckInstanceExist()
+	{
+		return mInstance;
+	}
+	#endregion Singleton
+
+	[Header("Settings")]
+	public GameObject gameManagerPrefab;
+	public GameObject soundManagerPrefab;
+    public GameObject objectPoolerPrefab;
+
+
+	[Header("In-Scene Managers")]
+	public SoundManagerScript soundManager;
+	public GameManagerScript gameManager;
+    public ObjectPooler objectPooler;
+}
