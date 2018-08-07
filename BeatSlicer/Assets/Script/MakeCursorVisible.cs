060329d5ebcdc01028560167eb74afb5c16c613e
KEBS\User <nubfight@gmail.com>
2018-08-07 21:27:36 +0800

    Tweaked WIn/Defeat Screen, Added KDU Logo + Team Logo, Added Tutorial Scene

---
+++ BeatSlicer/Assets/Script/MakeCursorVisible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeCursorVisible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+	}
+}
