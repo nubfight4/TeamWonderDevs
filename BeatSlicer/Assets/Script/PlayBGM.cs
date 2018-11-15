bdaf46e37bbd71578a3aa16bdca2bede656d9115
Wongfive <0112273@kdu-online.com>
2018-11-15 19:09:59 +0800

    - Added a simple Tutorial Stage - Added the necessary simple mechanics for the Tutorial Stage to work

--- BeatSlicer/Assets/Script/PlayBGM.cs
+++ BeatSlicer/Assets/Script/PlayBGM.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayBGM : MonoBehaviour
 {
     public BossAIScript bossAIScript;
+    private Scene currentSceneName;
 
     void Start()
     {
-        SoundManagerScript.mInstance.PlayBGM(AudioClipID.BGM_SECTION_1_INTRO);
-        SoundManagerScript.mInstance.bgmAudioSource.loop = false;
-        SoundManagerScript.mInstance.bgmAudioSource.volume = 0.0f;
+        currentSceneName = SceneManager.GetActiveScene();
+
+        if(currentSceneName.name == "Tutorial Level Scene")
+        {
+            SoundManagerScript.mInstance.PlayBGM(AudioClipID.BGM_INGAME_1); // Temporary Music Until Decided
+            SoundManagerScript.mInstance.bgmAudioSource.loop = true;
+        }
+        else
+        {
+            SoundManagerScript.mInstance.PlayBGM(AudioClipID.BGM_SECTION_1_INTRO);
+            SoundManagerScript.mInstance.bgmAudioSource.loop = false;
+            SoundManagerScript.mInstance.bgmAudioSource.volume = 0.0f;
+        }
     }
 
 
     void Update()
     {
+        currentSceneName = SceneManager.GetActiveScene();
+
         if(SoundManagerScript.mInstance.bgmAudioSource.isPlaying == false)
         {
             if(bossAIScript.currentMovementPattern == BossAIScript.MovementPattern.MOVE_PATTERN_1 && bossAIScript.currentMovementPattern != BossAIScript.MovementPattern.BOSS_STUN)
---
+++ BeatSlicer/Assets/Script/TutorialMovementCollisionBoxScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMovementCollisionBoxScript : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject bulletDispenser;
+    private bool combatTutorialHasStarted = false;
+    public float centerpointDistanceChecker;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	void Update()
+    {
+        if(combatTutorialHasStarted == false)
+        {
+            centerpointDistanceChecker = Vector3.Distance(player.transform.position,transform.position);
+
+            if(centerpointDistanceChecker >= 5.0f)
+            {
+                bulletDispenser.SetActive(true);
+
+                combatTutorialHasStarted = true;
+            }
+        }
+    }
+}
