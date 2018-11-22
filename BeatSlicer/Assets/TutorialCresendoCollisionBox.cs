a1fcc4edc58e81e524c7393abf5c16eb92efbbcc
Wongfive <0112273@kdu-online.com>
2018-11-22 16:04:13 +0800

    - Tutorial Stage Stuff

---
+++ BeatSlicer/Assets/TutorialCresendoCollisionBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCresendoCollisionBox : MonoBehaviour
+{
+    private GameObject player;
+    private bool cresendoHasCharged = false;
+    PlayerModelScript playerModelScript;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start()
+    {
+        playerModelScript = player.GetComponent<PlayerModelScript>();
+    }
+	
+
+	void Update()
+    {
+		
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(cresendoHasCharged == false)
+        {
+            if(other.tag == "Player")
+            {
+                playerModelScript.charge = playerModelScript.maxCharge;
+                cresendoHasCharged = true;
+            }
+        }
+    }
+}
