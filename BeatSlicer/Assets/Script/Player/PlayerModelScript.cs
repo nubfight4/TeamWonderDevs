2f323ac7334213d20e6f48e10ccf76279253c388
KEBS\User <nubfight@gmail.com>
2018-12-12 16:15:25 +0800

    Bugfixes

--- BeatSlicer/Assets/Script/Player/PlayerModelScript.cs
+++ BeatSlicer/Assets/Script/Player/PlayerModelScript.cs
@@ -89,7 +89,7 @@ public class PlayerModelScript : MonoBehaviour {
         #endregion
 
         #region Attack Function
-        if (Input.GetButtonDown("Attack") && !isPlayerChargeSlashing && Time.timeScale != 0)
+        if (Input.GetButtonDown("Attack") && !isPlayerChargeSlashing && health > 0 && Time.timeScale != 0)
         {
             {
                 animator.Play("AttackAnimationClip");
